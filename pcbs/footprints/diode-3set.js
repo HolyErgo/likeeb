11a3fc539beea3d1c997bdd4babd160b66a64e25
Sergey Nikiforov <ser.nikiforov@gmail.com>
2024-01-27 23:12:58 +0100

    diode set update with via and smaller width

--- pcbs/footprints/diode-3set.js
+++ pcbs/footprints/diode-3set.js
@@ -22,7 +22,8 @@ module.exports = {
     to1: {type: 'net', value: ''},
     to2: {type: 'net', value: ''},
     to3: {type: 'net', value: ''},
-    pitch: 2.5
+    pitch: 2.5,
+    via: false
   },
   body: p => {
     const count = 3;
@@ -41,27 +42,30 @@ module.exports = {
 
     function diode(from, to, x) {
 
+      const diode_to_via = `(via (at ${p.xy(x, 1.65)}) (size 0.6) (drill 0.3) (layers "F.Cu" "B.Cu") (net ${to.index}))`
+
       return `
         ${'' /*  */}
         (module SmdDiode (layer B.Cu)
           ${p.at /* parametric position */}
 
           ${''/* diode symbol */}
-          (fp_line (start ${x+1} -1.65) (end ${x+1} +2.45) (layer B.SilkS) (width 0.1))
-          (fp_line (start ${x+1} +2.45) (end ${x-1} +2.45) (layer B.SilkS) (width 0.1))
-          (fp_line (start ${x-1} +2.45) (end ${x-1} -1.65) (layer B.SilkS) (width 0.1))
+          (fp_line (start ${x+0.9} -1.65) (end ${x+0.9} +2.45) (layer B.SilkS) (width 0.1))
+          (fp_line (start ${x+0.9} +2.45) (end ${x-0.9} +2.45) (layer B.SilkS) (width 0.1))
+          (fp_line (start ${x-0.9} +2.45) (end ${x-0.9} -1.65) (layer B.SilkS) (width 0.1))
       
           ${''/* SMD pads on back side */}
           (pad 1 smd rect (at ${x} -1.65 ${p.rot}) (size 1.2 1.2) (layers B.Cu B.Paste B.Mask) ${from.str})
           (pad 2 smd rect (at ${x} +1.65 ${p.rot}) (size 1.2 1.2) (layers B.Cu B.Paste B.Mask) ${to.str})
         
-          (fp_line (start ${x-1} -2.45) (end ${x+1} -2.45) (layer B.CrtYd) (width 0.05))
-          (fp_line (start ${x+1} -2.45) (end ${x+1} +2.45) (layer B.CrtYd) (width 0.05))
-          (fp_line (start ${x+1} +2.45) (end ${x-1} +2.45) (layer B.CrtYd) (width 0.05))
-          (fp_line (start ${x-1} +2.45) (end ${x-1} -2.45) (layer B.CrtYd) (width 0.05))
+          (fp_line (start ${x-0.85} -2.45) (end ${x+0.85} -2.45) (layer B.CrtYd) (width 0.05))
+          (fp_line (start ${x+0.85} -2.45) (end ${x+0.85} +2.45) (layer B.CrtYd) (width 0.05))
+          (fp_line (start ${x+0.85} +2.45) (end ${x-0.85} +2.45) (layer B.CrtYd) (width 0.05))
+          (fp_line (start ${x-0.85} +2.45) (end ${x-0.85} -2.45) (layer B.CrtYd) (width 0.05))
         )
 
 
+        ${p.via ? diode_to_via : ''}
       `
     }
 
